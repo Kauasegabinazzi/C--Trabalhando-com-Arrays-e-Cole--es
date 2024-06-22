9606faa90228b047ac838e7989c010809176520e
Kauasegabinazzi <101999858+Kauasegabinazzi@users.noreply.github.com>
2024-06-21 22:45:02 -0300

    2-commit

--- program.c
+++ program.c
@@ -30,3 +30,26 @@ void TestaArrayInt()
     int media = acumulador / idades.Length;
     Console.WriteLine($"media = {media}");
 }
+
+void TestaBusca()
+{
+    string[] arrayPalavras = new string[5];
+
+    for (int i = 0; i< arrayPalavras.Length; i++)
+    {
+        Console.Write($"Digite {i+1} a palavra:");
+        arrayPalavras[i] = Console.ReadLine();
+    }
+
+    Console.Write("Digite a palavra a ser encontrada:");
+    var busca = Console.ReadLine();
+
+    foreach(string current in arrayPalavras)
+    {
+        if (current.Equals(busca)){
+            Console.WriteLine($"Palavra encontrada = {busca}");
+            break;
+        }
+ 
+    }
+}
