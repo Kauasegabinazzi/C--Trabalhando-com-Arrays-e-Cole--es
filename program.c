027041c203bde7acef8a7c663293b6cc8f959f5d
Kauasegabinazzi <101999858+Kauasegabinazzi@users.noreply.github.com>
2024-06-22 22:30:39 -0300

    3-commit

--- program.c
+++ program.c
@@ -4,7 +4,17 @@ int idade = 30;
 int idade1 = 40;
 int idade2 = 25;
 
-TestaArrayInt();
+Array amostra = Array.CreateInstance(typeof(double), 5);
+
+amostra.SetValue(5.9, 0);
+amostra.SetValue(3.5, 1);
+amostra.SetValue(23.5, 2);
+amostra.SetValue(7.7, 3);
+amostra.SetValue(1.2, 4);
+
+//TestaArrayInt();
+//TestaBusca();
+testaMediana(amostra);
 
 void TestaArrayInt()
 {
@@ -52,4 +62,21 @@ void TestaBusca()
         }
  
     }
+}
+
+void testaMediana(Array array)
+{
+    if((array == null) || (array.Length == 0)){
+        Console.WriteLine("Array vazio");
+    }
+
+    double[] numeroOrder = (double[])array.Clone();
+    Array.Sort(numeroOrder);
+
+    int tamanho = numeroOrder.Length;
+    int meio = tamanho / 2;
+
+    double mediana = (tamanho % 2 != 0) ? numeroOrder[meio] : (numeroOrder[meio] + numeroOrder[meio - 1]) / 2;
+
+    Console.WriteLine($"Mediana é igual a {mediana}");
 }
